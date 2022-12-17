09b15f8b577f6bea5f95b0e6e07da7eee7113a59
Ivan <iv4n.eng.2@gmail.com>
2022-12-17 16:26:54 +0800

    completed dockerfile

--- src/main/java/sg/oracle/assessment/solution/controller/FibonacciController.java
+++ src/main/java/sg/oracle/assessment/solution/controller/FibonacciController.java
@@ -5,6 +5,7 @@ import java.math.BigInteger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +14,13 @@ import sg.oracle.assessment.solution.model.Elements;
 import sg.oracle.assessment.solution.model.Fibonacci;
 import sg.oracle.assessment.solution.service.FibonacciService;
 
-
 // Since we need to return Fibonacci sequence up to 100 elements, use BigInteger.
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class FibonacciController {
 
     @Autowired
     FibonacciService fibonacciService;
-    
 
     @GetMapping("/fibonacci")
     public ResponseEntity generateSequence(@RequestBody Elements elements) {
