e1dec4ac73cd1533a34800498da04093ebec57a8
Ivan <iv4n.eng.2@gmail.com>
2022-12-17 21:01:13 +0800

    added mapping for pathvar

--- src/main/java/sg/oracle/assessment/solution/controller/FibonacciController.java
+++ src/main/java/sg/oracle/assessment/solution/controller/FibonacciController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ import sg.oracle.assessment.solution.model.Fibonacci;
 import sg.oracle.assessment.solution.service.FibonacciService;
 
 // Since we need to return Fibonacci sequence up to 100 elements, use BigInteger.
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000") // for ReactJS client to connect 
 @RestController
 public class FibonacciController {
 
@@ -26,6 +27,17 @@ public class FibonacciController {
     public ResponseEntity generateSequence(@RequestBody Elements elements) {
         int elementsInt = elements.getElements();
         BigInteger[] fibonacciSequence = fibonacciService.getFibonacciSequence(elementsInt);
+        BigInteger[] sortedSequence = fibonacciService.sortSequence(fibonacciSequence);
+
+        Fibonacci fibonacci = new Fibonacci(fibonacciSequence, sortedSequence);
+
+        return ResponseEntity.status(HttpStatus.OK).body(fibonacci);
+    }
+
+    @GetMapping("/fibonacci/{elements}")
+    public ResponseEntity generateSequenceQueryParam(@PathVariable("elements") int elements) {
+        
+        BigInteger[] fibonacciSequence = fibonacciService.getFibonacciSequence(elements);
         BigInteger[] sortedSequence = fibonacciService.sortSequence(fibonacciSequence);
 
         Fibonacci fibonacci = new Fibonacci(fibonacciSequence, sortedSequence);
